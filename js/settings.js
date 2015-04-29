4a17d8d655a8d13722ad9f1c25193b47512886d4
intrepidwebstudio <sunil@inpws.com>
2015-04-29 18:25:30 +0530

    ATOZ DEV

---
+++ js/settings.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var ajaxpath="http://192.168.1.13/atoz/app/";
+var imagepath="http://192.168.1.13/atoz/";
+
+//var ajaxpath="http://www.safelearners.com/atoz/app/";
+//var imagepath="http://www.safelearners.com/atoz/";
+var	globalusername='';
+var	globaluserid='';
+var	globaluserphone='';
+var filtermenu_array=[];
+var cart_array=[];
+
+var globalmenuObj;
+
+var final_cartjsonObj=[];
+var MenuTypeId='';
+var MenuDate='';
+var MenuDateId='';
+var TotalCartAmount='';
